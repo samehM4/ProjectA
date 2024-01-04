3ce642324e5aca5f8aa6235fc15cafe2dbc0a558
samehM4 <155477674+samehM4@users.noreply.github.com>
2024-01-04 18:25:01 -0500

    Update DemoA.java

--- src/A/DemoA.java
+++ src/A/DemoA.java
@@ -14,4 +14,8 @@ public class DemoA {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+
+	//Coming from github
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
 }
