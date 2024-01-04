38b7078fa9b944e8ffdb4a3859a80aab926a53ed
samehmahfooz <samehmahfooz@macbook-pro-3>
2024-01-04 17:15:42 -0500

    Initial Commit

---
+++ src/A/DemoA.java
@@ -0,0 +1,12 @@
+package A;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Sprint 1 work...");
+		System.out.println("Sprint 2 work...");
+	}
+}
